6aed3d8c646ce089afc6fb2ae409474aca4c9966
Derek Vaughn <dvaughn@mit.edu>
2013-05-08 13:38:47 -0400

    pushing

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,6 +1,6 @@
 class Athlete < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :email, :sport, :height, :weight, :hometown, :status, :pic, :temp_id
-  has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/public/images/john.jpg"
+  has_attached_file :pic, :styles => { :medium => "195x173#{}", :thumb => "100x140>" }, :default_url => "/public/images/john.jpg"
   belongs_to :trainer
   has_one :workout
   has_many :notes
